4eaf5381ef726449b1141faae1902f710b67d693
Tharupathi Bandara <tharupathi19@gmail.com>
2026-01-11 14:24:19 +0530

    ali

--- src/dao/CourseDAO.java
+++ src/dao/CourseDAO.java
@@ -45,11 +45,16 @@ public class CourseDAO {
     }
 
     public boolean deleteCourse(String id) {
+
         String sql = "DELETE FROM courses WHERE course_id=?";
+
         try (Connection conn = DBConnection.getConnection();
+
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
             pstmt.setString(1, id);
             return pstmt.executeUpdate() > 0;
+
         } catch (SQLException e) { e.printStackTrace(); return false; }
     }
 }
